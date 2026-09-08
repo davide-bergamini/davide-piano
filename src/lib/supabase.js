89af6e493e3620b0006594c1d5d9b039a2444fd6
Davide Bergamini <davide.bergamini@ymail.com>
2026-09-08 06:35:51 +0200

    Aggiorna configurazione Supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://hfbwiqoheyzkzminoebb.supabase.co'
-const supabaseAnonKey = 'sb_publishable_amHR9NdyAtRSNcxw0ljMBA_OFyBE8_I'
+const supabaseUrl = 'https://ijiallyxrxewbmjmqaud.supabase.co'
+const supabaseAnonKey = 'sb_publishable_cCsEYP_I29SrQ7e3tk4nRA_Z2b88WBv'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
